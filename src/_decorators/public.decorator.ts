5eaa085422dc548882e03386fe9b90270347a751
Tacx <12997062+tacxou@users.noreply.github.com>
2024-06-19 14:18:00 +0200

    feat: Add public decorator for unprotected routes

---
+++ src/_decorators/public.decorator.ts
@@ -0,0 +1,5 @@
+import { applyDecorators, SetMetadata } from '@nestjs/common'
+
+export const META_UNPROTECTED = 'unprotected'
+
+export const Public = () => applyDecorators(SetMetadata(META_UNPROTECTED, true))
