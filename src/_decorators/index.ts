a6d35e885cde6def610cd2ad53ee11a1db195e07
Tacx <12997062+tacxou@users.noreply.github.com>
2024-06-24 12:40:25 +0200

    chore: Update _decorators module to lazy load 'request-ip' dependency

--- src/_decorators/index.ts
+++ src/_decorators/index.ts
@@ -1,1 +1,2 @@
+
 export * from './real-ip.decorator'
--- src/_decorators/real-ip.decorator.ts
+++ src/_decorators/real-ip.decorator.ts
@@ -1,7 +1,20 @@
 import { getClientIp } from 'request-ip'
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 
-// noinspection JSUnusedGlobalSymbols
+let getClientIp: (request: any) => string | null
+
+(async () => {
+  try {
+    const requestIpModule = await import('request-ip')
+    getClientIp = requestIpModule.getClientIp
+  } catch (error) {
+    getClientIp = (_: any): string | null => {
+      console.error("Le package 'request-ip' n'est pas installé. Veuillez l'installer pour utiliser le décorateur RealIp.")
+      return null
+    }
+  }
+})()
+
 export const RealIp = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest()
   return getClientIp(request)
